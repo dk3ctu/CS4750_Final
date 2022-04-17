595caff80b0e18d8c88bc3b2da22dc4ab934c4c3
Vivine Zheng <zhengvivine@gmail.com>
2022-04-17 17:17:01 -0400

    improvements

--- templates/pokedex.php
+++ templates/pokedex.php
@@ -101,113 +101,117 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <p class="lead">View the Pokédex here!</p>
         <form method="post">
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="Search for a Pokemon by name..." autofocus id="searchValue" name="searchValue" onkeypress="checkForEnter(event)" />
+                <input type="text" class="form-control" placeholder="Search for a Pokemon by name..." autofocus id="searchValue" name="searchValue" onkeypress="checkForEnter(event)" <?php if (isset($_POST['searchValue'])) {
+                                                                                                                                                                                            echo "value = '";
+                                                                                                                                                                                            echo $_POST['searchValue'];
+                                                                                                                                                                                            echo "'";
+                                                                                                                                                                                        } ?> />
                 <input type="submit" class="btn btn-primary" value="Search" id="searchBtn" name="searchBtn" />
 
             </div>
             <div class="mt-4">
                 <p>Include only these types:</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="normal" value="normal">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="normal" value="normal" <?php if (isset($_POST['type_list']) && in_array("normal", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="normal">Normal</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="fighting" value="fighting">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="fighting" value="fighting" <?php if (isset($_POST['type_list']) && in_array("fighting", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="fighting">Fighting</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="flying" value="flying">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="flying" value="flying" <?php if (isset($_POST['type_list']) && in_array("flying", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="flying">Flying</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="poison" value="poison">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="poison" value="poison" <?php if (isset($_POST['type_list']) && in_array("poison", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="poison">Poison</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="ground" value="ground">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="ground" value="ground" <?php if (isset($_POST['type_list']) && in_array("ground", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="ground">Ground</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="rock" value="rock">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="rock" value="rock" <?php if (isset($_POST['type_list']) && in_array("rock", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="rock">Rock</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="bug" value="bug">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="bug" value="bug" <?php if (isset($_POST['type_list']) && in_array("bug", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="bug">Bug</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="ghost" value="ghost">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="ghost" value="ghost" <?php if (isset($_POST['type_list']) && in_array("ghost", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="ghost">Ghost</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="steel" value="steel">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="steel" value="steel" <?php if (isset($_POST['type_list']) && in_array("steel", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="steel">Steel</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="fire" value="fire">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="fire" value="fire" <?php if (isset($_POST['type_list']) && in_array("fire", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="fire">Fire</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="water" value="water">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="water" value="water" <?php if (isset($_POST['type_list']) && in_array("water", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="water">Water</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="grass" value="grass">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="grass" value="grass" <?php if (isset($_POST['type_list']) && in_array("grass", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="grass">Grass</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="electric" value="electric">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="electric" value="electric" <?php if (isset($_POST['type_list']) && in_array("electric", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="electric">Electric</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="psychic" value="psychic">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="psychic" value="psychic" <?php if (isset($_POST['type_list']) && in_array("psychic", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="psychic">Psychic</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="ice" value="ice">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="ice" value="ice" <?php if (isset($_POST['type_list']) && in_array("ice", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="ice">Ice</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="dragon" value="dragon">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="dragon" value="dragon" <?php if (isset($_POST['type_list']) && in_array("dragon", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="dragon">Dragon</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="dark" value="dark">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="dark" value="dark" <?php if (isset($_POST['type_list']) && in_array("dark", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="dark">Dark</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="type_list[]" id="fairy" value="fairy">
+                    <input class="form-check-input" type="checkbox" name="type_list[]" id="fairy" value="fairy" <?php if (isset($_POST['type_list']) && in_array("fairy", $_POST['type_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="fairy">Fairy</label>
                 </div>
             </div>
             <div class="mt-4">
                 <p>Include only Pokemon from these generations:</p>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="1" value="1">
+                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="1" value="1" <?php if (isset($_POST['type_list']) && in_array("1", $_POST['gen_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="normal">1</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="2" value="2">
+                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="2" value="2" <?php if (isset($_POST['type_list']) && in_array("2", $_POST['gen_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="fighting">2</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="3" value="3">
+                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="3" value="3" <?php if (isset($_POST['type_list']) && in_array("3", $_POST['gen_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="flying">3</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="4" value="4">
+                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="4" value="4" <?php if (isset($_POST['type_list']) && in_array("4", $_POST['gen_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="poison">4</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="5" value="5">
+                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="5" value="5" <?php if (isset($_POST['type_list']) && in_array("5", $_POST['gen_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="ground">5</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="6" value="6">
+                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="6" value="6" <?php if (isset($_POST['type_list']) && in_array("6", $_POST['gen_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="rock">6</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="7" value="7">
+                    <input class="form-check-input" type="checkbox" name="gen_list[]" id="7" value="7" <?php if (isset($_POST['type_list']) && in_array("7", $_POST['gen_list'])) echo "checked"; ?>>
                     <label class="form-check-label" for="rock">7</label>
                 </div>
             </div>
